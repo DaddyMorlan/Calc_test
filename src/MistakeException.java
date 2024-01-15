cbd927515a23c14e7ed9d6410f27470b845f2214
Artem Shkret <shkret@yahoo.com>
2024-01-16 03:13:19 +0500

    Initial commit

---
+++ src/MistakeException.java
@@ -0,0 +1,5 @@
+public class MistakeException extends Exception{
+    public MistakeException (String message){
+        super(message);;
+    }
+}
---
+++ src/Run.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Run {
+    static String calculator(String input) throws MistakeException {
+           try {
+                String[] tokens = input.split(" ", 3);//делим одну строку на число-знак-число, ввод через пробел как в примере
+                int a = Integer.parseInt(tokens[0]); // 0-й символ - первое число
+                String znak = tokens[1]; // 1-й символ - знак
+                int b = Integer.parseInt(tokens[2]); // 2-й символ - второе число
+                int result; // результат
+                if ((a <= 10 && a >= 1 && b <= 10 && b >= 1)) {
+                    switch (znak) {
+                        case "+":
+                            result = a + b;
+                            break;
+                        case "-":
+                            result = a - b;
+                            break;
+                        case "/":
+                            result = a / b;
+                            break;
+                        case "*":
+                            result = a * b;
+                            break;
+                        default:
+                            throw new MistakeException("формат математической операции не удовлетворяет заданию - целые числа, два операнда и один оператор (+, -, /, *)");
+                    }
+                } else {
+                    throw new MistakeException("Только числа от 1 до 10");
+                }
+                return Integer.toString(result); // переводим число result в строку и выводим result
+            } catch (Exception e){
+                throw new MistakeException("формат математической операции не удовлетворяет заданию - целые числа, два операнда и один оператор (+, -, /, *)");
+            }
+    }
+}
+
+class Main {
+    public static void main(String[] args) throws MistakeException{
+        Scanner scanner = new Scanner(System.in); //обозначение объекта сканнер
+        System.out.println("Введите Ваш пример:"); // приветственная строка
+        String input = scanner.nextLine(); // запрос ввода
+        System.out.println("Ответ: " + Run.calculator(input));
+    }
+}
